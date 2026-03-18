b5f90e7836900077df158f55a392028e9672948d
sujay <jirpuresujay@gmail.com>
2026-03-18 23:22:59 +0530

    api key update

--- api/contact.js
+++ api/contact.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
 
     const response = await resend.emails.send({
       from: "ATIN <onboarding@resend.dev>",
-      to: "natureprocreators@gmail.com",
+      to: "sujayjirapure03@gmail.com",
       subject: "New Plastic Waste Inquiry",
       html: `
         <h2>New Inquiry</h2>
